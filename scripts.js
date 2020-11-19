8be8c85db7c3e80389ca75532138687da295d177
angelavale13 <gypsy.aav@gmail.com>
2020-11-18 20:50:17 -0700

    Add sections to html product desc and js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 //Author: Angela Valenzuela
+
+//toggle button start
 document.getElementById('toggleMe').addEventListener('click', function(){
 	this.classList.toggle('metal');
+});
+
+
+//Display by default on page load and only one product should ever display on the screen at a time
+document.getElementById('viewDetails').addEventListener('click', function(){
+	this.classList.toggle('hide');
 });
